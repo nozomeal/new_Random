ef51c56afd5811fa95f5fda368e5c521e4d1c198
Nozomi Sakamoto <nozomeal.7msk@gmail.com>
2015-07-13 16:22:12 +0900

    more

--- Random_game/ViewController.swift
+++ Random_game/ViewController.swift
@@ -13,18 +13,6 @@ class ViewController: UIViewController {
     
     //ラベルのアウトレット接続
     @IBOutlet weak var label:UILabel!
-    //4桁の数字を入れる配列を準備する
-    var combination : [Int] = [1,2,3,4]
-    
-   
-    //１〜４までの数をランダムに決める
-    var rand1 = Int(arc4random_uniform(4)+1)
-    var rand2 = Int(arc4random_uniform(4)+1)
-    var rand3 = Int(arc4random_uniform(4)+1)
-    var rand4 = Int(arc4random_uniform(4)+1)
-    
-    
-    
 
     
     //ボタンの初期設定
@@ -33,25 +21,27 @@ class ViewController: UIViewController {
     private var button3: UIButton!
     private var button4: UIButton!
     
+    //4桁の数字を入れる配列を準備する
+    var rand : [Int] = [0,0,0,0]
     
-    
-
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
-        //Int型であるrandをString型に変換する
-        var a = String(rand1)
-        var b = String(rand2)
-        var c = String(rand3)
-        var d = String(rand4)
         
         //配列の要素数を取得する
-        var count: Int = combination.count
-    
+        var n: Int = rand.count
+        
+        //１〜４までの数をランダムに決める
+         rand[0] = Int(arc4random_uniform(4)+1)
+         rand[1] = Int(arc4random_uniform(4)+1)
+         rand[2] = Int(arc4random_uniform(4)+1)
+         rand[3] = Int(arc4random_uniform(4)+1)
+        
+        
         //ランダムな４桁の数字をラベルに表示する
-        label.text = String(stringInterpolation: a,b,c,d)
+        label.text = "\(rand[0])\(rand[1])\(rand[2])\(rand[3])"
 
         
         
@@ -143,125 +133,118 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
         
     }
-    
+
     
     
     
     internal func onClickbutton1(button1: UIButton){
-        //Int型であるrandをString型に変換する
-        var a = String(rand1)
-        var b = String(rand2)
-        var c = String(rand3)
-        var d = String(rand4)
         
+        //配列の要素数を取得する
+        var n: Int = rand.count
         
-        //もしrand1が１で、他のrandに　１があれば
-        if rand1 == button1.tag && (rand2 == button1.tag || rand3 == button1.tag || rand4 == button1.tag) {
-            //rand1は消えるので残りのrand2,rand3,rand4を表示する
-            label.text = String (stringInterpolation: b,c,d)
-            //もし rand2が1で
-        }else if rand1 == button1.tag && rand2 != button1.tag && rand3 != button1.tag && rand4 != button1.tag {
-            //rand1は消えるので残りのrand2,rand3,rand4を表示する
-            label.text = String (stringInterpolation: b,c,d)
-            combination.removeAtIndex(0)
-        }else if rand2 == button1.tag && (rand3 == button1.tag || rand4 == button1.tag) {
-            ///rand2は消えるので残りのrand3,rand4を表示する
-            label.text = String (stringInterpolation: c,d)
-        }else if rand2 == button1.tag && rand3 != button1.tag && rand4 != button1.tag {
-            ///rand2は消えるので残りのrand3,rand4を表示する
-            label.text = String (stringInterpolation: c,d)
-            combination.removeAtIndex(0)
-        }else if rand3 == button1.tag && rand4 == button1.tag {
-            ///rand3は消えるので残りのrand4を表示する
-            label.text = String (stringInterpolation: d)
-        }else if rand3 == button1.tag && rand4 != button1.tag {
-            ///rand3は消えるので残りのrand4を表示する
-            label.text = String (stringInterpolation: d)
-            combination.removeAtIndex(0)
-        }else if rand4 == button1.tag {
-            label.text = String (0)
+        //要素の数が４個　かつ　rand[0]が１のとき
+        if n == 4 && rand[0] == button1.tag {
+                //残りの３桁のみとなる
+                label.text = "\(rand[1])\(rand[2])\(rand[3])"
+                //rand[0]の要素を削除
+                rand.removeAtIndex(rand[1])
+            
+            //要素の数が３個　かつ　rand[1]が１のとき
+        }else if n == 3 && rand[1] == button1.tag {
+                //残り２桁のみとなる
+                label.text = "\(rand[2])\(rand[3])"
+                //rand[1]の要素を削除
+                rand.removeAtIndex(rand[1])
+            
+            //要素の数が２個 かつ　rand[2]が１のとき
+        }else if n == 2 && rand[2] == button1.tag {
+                //残りの１桁を表示
+                label.text = "\(rand[3])"
+                //rand[2]の要素を削除
+                rand.removeAtIndex(rand[2])
+    
+            //要素の個数が１こ かつ　rand[3]のとき
+        }else if n == 1 && rand[3] == button1.tag {
+                //rand[3]の要素を削除
+                rand.removeAtIndex(rand[3])
             }
-        
-        
+            
         }
-        
+
+    
 
     
     internal func onClickbutton2(button2: UIButton){
-    //Int型であるrandをString型に変換する
-    var a = String(rand1)
-    var b = String(rand2)
-    var c = String(rand3)
-    var d = String(rand4)
+       
+        //配列の要素数を取得する
+        var n: Int = rand.count
+        
+        if n == 4 && rand[0] == button2.tag {
+                label.text = "\(rand[1])\(rand[2])\(rand[3])"
+                rand.removeAtIndex(rand[0])
     
-    if rand1 == button2.tag {
-        //rand1は消えるので残りのrand2,rand3,rand4を表示する
-        label.text = String (stringInterpolation: b,c,d)
-        //もし rand2が"1"ならば
-    }else if rand2 == button2.tag {
-        ///rand2は消えるので残りのrand3,rand4を表示する
-        label.text = String (stringInterpolation: c,d)
-    }else if rand3 == button2.tag {
-        ///rand3は消えるので残りのrand4を表示する
-        label.text = String (stringInterpolation: d)
-    }else if rand4 == button2.tag {
-        label.text = String (0)
-    }
-    //要素[2]を削除する
-    combination.removeAtIndex(1)
-    
+        }else if n == 3 && rand[1] == button2.tag {
+                label.text = "\(rand[2])\(rand[3])"
+                rand.removeAtIndex(rand[1])
+            
+        }else if n == 2 && rand [2] == button2.tag {
+                label.text = "\(rand[3])"
+                rand.removeAtIndex(rand[2])
+            
+        }else if n == 1 && rand[3] == button2.tag {
+                label.text = "\(rand[3])"
+                rand.removeAtIndex(rand[3])
+            
+        }
     
 }
     internal func onClickbutton3(button3: UIButton){
-    //Int型であるrandをString型に変換する
-    var a = String(rand1)
-    var b = String(rand2)
-    var c = String(rand3)
-    var d = String(rand4)
     
-    if rand1 == button3.tag {
-        //rand1は消えるので残りのrand2,rand3,rand4を表示する
-        label.text = String (stringInterpolation: b,c,d)
-        //もし rand2が"1"ならば
-    }else if rand2 == button3.tag {
-        ///rand2は消えるので残りのrand3,rand4を表示する
-        label.text = String (stringInterpolation: c,d)
-    }else if rand3 == button3.tag {
-        ///rand3は消えるので残りのrand4を表示する
-        label.text = String (stringInterpolation: d)
-    }else if rand4 == button3.tag {
-        label.text = String (0)
+        //配列の要素数を取得する
+        var n: Int = rand.count
+        
+        if n == 4 && rand[0] == button3.tag {
+                label.text = "\(rand[1])\(rand[2])\(rand[3])"
+                rand.removeAtIndex(rand[0])
+            
+        }else if n == 3 && rand[1] == button3.tag {
+                label.text = "\(rand[2])\(rand[3])"
+                rand.removeAtIndex(rand[1])
+            
+        }else if n == 2 && rand[3] == button3.tag {
+                label.text = "\(rand[3])"
+                rand.removeAtIndex(rand[2])
+        }else if n == 1 && rand[3] == button3.tag {
+                label.text = "\(rand[3])"
+                rand.removeAtIndex(rand[3])
+        }
     }
-    //要素[1]を削除する
-    combination.removeAtIndex(2)
     
-}
     internal func onClickbutton4(button4: UIButton){
-    //Int型であるrandをString型に変換する
-    var a = String(rand1)
-    var b = String(rand2)
-    var c = String(rand3)
-    var d = String(rand4)
-    
-    if rand1 == button4.tag {
-        //rand1は消えるので残りのrand2,rand3,rand4を表示する
-        label.text = String (stringInterpolation: b,c,d)
-        //もし rand2が"1"ならば
-    }else if rand2 == button4.tag {
-        ///rand2は消えるので残りのrand3,rand4を表示する
-        label.text = String (stringInterpolation: c,d)
-    }else if rand3 == button4.tag {
-        ///rand3は消えるので残りのrand4を表示する
-        label.text = String (stringInterpolation: d)
-    }else if rand4 == button4.tag {
-        label.text = String (0)
+        
+        //配列の要素数を取得する
+        var n: Int = rand.count
+        
+        
+        if n == 4 && rand[0] == button4.tag {
+                //rand1は消えるので残りのrand2,rand3,rand4を表示する
+                label.text = "\(rand[1])\(rand[2])\(rand[3])"
+                rand.removeAtIndex(rand[0])
+            
+        }else if n == 3 && rand[1] == button4.tag {
+                label.text = "\(rand[2])\(rand[3])"
+                rand.removeAtIndex(rand[1])
+        }else if n == 2 && rand [2] == button4.tag {
+                label.text = "\(rand[3])"
+                rand.removeAtIndex(rand[2])
+            
+        }else if n == 1 && rand[3] == button4.tag {
+                label.text = "\(rand[3])"
+                rand.removeAtIndex(rand[3])
+            }
+
+        
     }
-    //要素[1]を削除する
-    combination.removeAtIndex(3)
-    
+
 }
-    }
-
-
-
 
